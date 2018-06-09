c3eb6391c59bc128340f19e7e639764628a9ba95
Christopher Greaves <cjgreaves97@hotmail.co.uk>
2018-06-10 00:42:18 +0100

    Added util packages for directory archiving and compression

---
+++ boxup-server/gziputil/gziputil.go
@@ -0,0 +1,57 @@
+// This package was taken from http://blog.ralch.com/tutorial/golang-working-with-tar-and-gzip/
+
+package gziputil
+
+import (
+	"io"
+	"fmt"
+	"compress/gzip"
+	"path/filepath"
+	"os"
+)
+
+func gzipit(source, target string) error {
+	reader, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	filename := filepath.Base(source)
+	target = filepath.Join(target, fmt.Sprintf("%s.gz", filename))
+	writer, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	archiver := gzip.NewWriter(writer)
+	archiver.Name = filename
+	defer archiver.Close()
+
+	_, err = io.Copy(archiver, reader)
+	return err
+}
+
+func ungzip(source, target string) error {
+	reader, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	archive, err := gzip.NewReader(reader)
+	if err != nil {
+		return err
+	}
+	defer archive.Close()
+
+	target = filepath.Join(target, archive.Name)
+	writer, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	_, err = io.Copy(writer, archive)
+	return err
+}
